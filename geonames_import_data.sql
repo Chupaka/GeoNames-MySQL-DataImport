d117dc0dd8615594bd58a7f108da2d9b9056863f
Pavel Skuratovich <pavel.skuratovich@apalon.com>
2019-08-13 13:29:46 +0300

    Disable keys during LOAD DATA for faster load

--- geonames_import_data.sql
+++ geonames_import_data.sql
@@ -1,69 +1,92 @@
 SELECT '########## Loading allCountries.txt... ##########';
+ALTER TABLE geoname DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/allCountries.txt'
 INTO TABLE geoname
 CHARACTER SET 'utf8mb4'
 (geonameid, name, asciiname, alternatenames, latitude, longitude, fclass, fcode, country, cc2, admin1, admin2, admin3, admin4, population, elevation, gtopo30, timezone, moddate);
+ALTER TABLE geoname ENABLE KEYS;
 
 SELECT '########## Loading alternateNames.txt... ##########';
+ALTER TABLE alternatename DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/alternateNames.txt'
 INTO TABLE alternatename
 CHARACTER SET 'utf8mb4'
 (alternatenameid, geonameid, isoLanguage, alternateName, isPreferredName, isShortName, isColloquial, isHistoric);
+ALTER TABLE alternatename ENABLE KEYS;
 
 SELECT '########## Loading iso-languagecodes.txt... ##########';
+ALTER TABLE iso_languagecodes DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/iso-languagecodes.txt'
 INTO TABLE iso_languagecodes
 CHARACTER SET 'utf8mb4'
 IGNORE 1 LINES
 (iso_639_3, iso_639_2, iso_639_1, language_name);
+ALTER TABLE iso_languagecodes ENABLE KEYS;
 
 SELECT '########## Loading admin1CodesASCII.txt... ##########';
+ALTER TABLE admin1CodesAscii DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/admin1CodesASCII.txt'
 INTO TABLE admin1CodesAscii
 CHARACTER SET 'utf8mb4'
 (code, name, nameAscii, geonameid);
+ALTER TABLE admin1CodesAscii ENABLE KEYS;
 
 SELECT '########## Loading admin2Codes.txt... ##########';
+ALTER TABLE admin2Codes DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/admin2Codes.txt'
 INTO TABLE admin2Codes
 CHARACTER SET 'utf8mb4'
 (code, name, nameAscii, geonameid);
+ALTER TABLE admin2Codes ENABLE KEYS;
 
 SELECT '########## Loading hierarchy.txt... ##########';
+ALTER TABLE hierarchy DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/hierarchy.txt'
 INTO TABLE hierarchy
 CHARACTER SET 'utf8mb4'
 (parentId, childId, type);
+ALTER TABLE hierarchy ENABLE KEYS;
 
 SELECT '########## Loading featureCodes_en.txt... ##########';
+ALTER TABLE featureCodes DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/featureCodes_en.txt'
 INTO TABLE featureCodes
 CHARACTER SET 'utf8mb4'
 (code, name, description);
+ALTER TABLE featureCodes ENABLE KEYS;
 
 SELECT '########## Loading timeZones.txt... ##########';
+ALTER TABLE timeZones DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/timeZones.txt'
 INTO TABLE timeZones
 CHARACTER SET 'utf8mb4'
 IGNORE 1 LINES
 (timeZoneId, GMT_offset, DST_offset);
+ALTER TABLE timeZones ENABLE KEYS;
 
 SELECT '########## Loading countryInfo.txt... ##########';
+ALTER TABLE countryinfo DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/countryInfo.txt'
 INTO TABLE countryinfo
 CHARACTER SET 'utf8mb4'
 IGNORE 51 LINES	
 (iso_alpha2, iso_alpha3, iso_numeric, fips_code, name, capital, areaInSqKm, population, continent, tld, currency, currencyName, phone, postalCodeFormat, postalCodeRegex, languages, geonameid, neighbours, equivalentFipsCode);
+ALTER TABLE countryinfo ENABLE KEYS;
 
 SELECT '########## Loading continentCodes.txt... ##########';
+ALTER TABLE continentCodes DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'continentCodes.txt'
 INTO TABLE continentCodes
 CHARACTER SET 'utf8mb4'
 FIELDS TERMINATED BY ','
 (code, name, geonameId);
+ALTER TABLE continentCodes ENABLE KEYS;
 
 -- Postal Code Data. Ignore until we are able to download them
+SELECT '########## Loading allCountries.txt... ##########';
+ALTER TABLE postalCodes DISABLE KEYS;
 LOAD DATA LOCAL INFILE 'data/zip_codes/allCountries.txt'
 INTO TABLE postalCodes
 CHARACTER SET 'utf8mb4'
-(country, postal_code, name, admin1_name, admin1_code, admin2_name, admin2_code, admin3_name, admin3_code, latitude, longitude, accuracy)
+(country, postal_code, name, admin1_name, admin1_code, admin2_name, admin2_code, admin3_name, admin3_code, latitude, longitude, accuracy);
+ALTER TABLE postalCodes ENABLE KEYS;
